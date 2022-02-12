e22d1091dd7e09ad03afeca772fa02d98332df69
= <=>
2022-02-12 16:57:08 +0000

    add faq section

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h2>How do I start?</h2>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+            tempore!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h2>Daily Schedule</h2>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+            tempore!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h2>Different Payment Methods</h2>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+            tempore!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h2>What products do you offer?</h2>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+            tempore!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
