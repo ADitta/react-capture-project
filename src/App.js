87accb6c23acac5aee9a910c59744e289fe6b37c
= <=>
2022-02-12 17:29:41 +0000

    start styling with styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 // Import pages
+import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
+// GLobal Style
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
